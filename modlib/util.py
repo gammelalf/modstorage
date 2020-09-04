d249272a03a525fabc8a56aff73947c146c0e2ad
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-09-04 16:44:42 +0200

    Added type annotations and better docstrings to util.py

--- modlib/util.py
+++ modlib/util.py
@@ -4,17 +4,31 @@ import re
 from .config import config
 
 
-def storage_path(*paths):
+def storage_path(*paths: str) -> str:
     """
-    Create a path inside the mods' storage directory
+    Return the absolute path to something inside the storage dictionary.
+
+    Use `os.path.join` on the input with the storage directory as defined in the config first.
+
+    :param paths: paths to be joined
+    :type paths: str
+    :return: absolute path
+    :rtype: str
     """
     # abspath is used because the result could be used in symlinks
     return os.path.join(os.path.abspath(config.get("MODS", "directory")), *paths)
 
 
-def packs_path(*paths):
+def packs_path(*paths: str) -> str:
     """
-    Create a path inside the packs' storage directory
+    Return the path to something inside the packs dictionary.
+
+    Use `os.path.join` on the input with the packs directory as defined in the config first.
+
+    :param paths: paths to be joined
+    :type paths: str
+    :return: relative path
+    :rtype: str
     """
     return os.path.join(config.get("PACKS", "directory"), *paths)
 
