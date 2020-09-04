912d899e20bfab0952173cb98db5b47630e51a22
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-09-04 18:08:00 +0200

    Added some error checks to give nicer error messages

--- main.py
+++ main.py
@@ -68,6 +68,16 @@ def main():
     # Parse Arguments
     args = parser.parse_args()
 
+    def check_version():
+        """Check whether a version was specified"""
+        if args.version is None:
+            parser.error("argument --version is required")
+
+    def check_mod():
+        """Check whether a mod was specified"""
+        if args.mod is None:
+            parser.error("argument --mod is required")
+
     # Execture Hidden Commands
     if args.list_mods:
         mods = os.listdir(storage_path())
@@ -82,12 +92,16 @@ def main():
     if args.new_mod:
         args.mod = Mod.new_mod(args.new_mod)
     if args.add_file:
+        check_mod()
+        check_version()
         args.mod.set_file(args.add_file, args.version)
     if args.add_dependencies:
+        check_mod()
         if args.version and args.mod.get("dependencies") is args.mod.get("dependencies", args.version):
             args.mod.set("dependencies", [], args.version)
         args.mod.get("dependencies", args.version).extend(args.add_dependencies)
     if args.read:
+        parser.error("argument --read is not supported at the moment")
         with Zipfile() as jar:
             with jar.open("mcmod.info") as info:
                 for key, value in json.load(info)[0].items():
@@ -97,10 +111,13 @@ def main():
 
     # Execute Pack Commands
     if args.new_pack:
+        check_version()
         args.pack = Pack(*args.new_pack, args.version)
     if args.add:
+        check_mod()
         args.pack.add(args.mod)
     if args.remove:
+        check_mod()
         args.pack.remove(args.mod)
     if args.autoremove:
         args.pack.autoremove()
